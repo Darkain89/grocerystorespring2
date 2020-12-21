ece48b70abc2c8a251bf16d6da780a85292ebde0
David Oster <paspa@hotmail.com>
2020-12-21 16:02:12 +0200

    REST API - Customer
    - fully implemented but without validations in every method

--- src/main/java/org/pcedu/grocerystorespring2/controllers/CustomerRestController.java
+++ src/main/java/org/pcedu/grocerystorespring2/controllers/CustomerRestController.java
@@ -6,18 +6,20 @@
 package org.pcedu.grocerystorespring2.controllers;
 
 import java.util.List;
-import javax.validation.Valid;
 import org.pcedu.grocerystorespring2.entities.Customer;
 import org.pcedu.grocerystorespring2.entities.dto.CustomerDTO;
 import org.pcedu.grocerystorespring2.entities.dto.CustomerOrderDetailsDTO;
 import org.pcedu.grocerystorespring2.entities.dto.CustomerOrdersDTO;
 import org.pcedu.grocerystorespring2.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,33 +35,26 @@ public class CustomerRestController {
     @Autowired
     CustomerService customerService;
 
-    @GetMapping("/hello")
-    public String hello() {
-        return ("hello");
-    }
-
     @RequestMapping(value = {"/{id}"})
     public CustomerDTO showCustomer(ModelMap view, @PathVariable String id) {
         view.addAttribute("projectName", "Grocery Store");
         CustomerDTO customerDTO = customerService.findByIdDTO(Integer.parseInt(id));
         return (customerDTO);
     }
-    
-    
+
     @GetMapping("/{id}/orders")
     public CustomerOrdersDTO showCustomerOrders(ModelMap view, @PathVariable String id) {
         view.addAttribute("projectName", "Grocery Store");
         CustomerOrdersDTO customerOrderDTO = customerService.findOrdersByCustomerIdDTO(Integer.parseInt(id));
-        return(customerOrderDTO);
+        return (customerOrderDTO);
     }
-    
+
     // @GetMapping("/api/customer/{id}/orders/details")
     public CustomerOrderDetailsDTO showCustomersOrdersDetails(@PathVariable String id) {
-        CustomerOrderDetailsDTO customerOrderDetailsDTO = 
-                customerService.findOrdersAndDetailsByCustomerIdDTO(Integer.parseInt(id));
-        return(customerOrderDetailsDTO);
+        CustomerOrderDetailsDTO customerOrderDetailsDTO
+                = customerService.findOrdersAndDetailsByCustomerIdDTO(Integer.parseInt(id));
+        return (customerOrderDetailsDTO);
     }
-    
 
     @RequestMapping("/all")
     public String showCustomers(ModelMap view) {
@@ -70,51 +65,46 @@ public class CustomerRestController {
         return ("customer");
     }
 
-    @RequestMapping("/new")
-    public String newCustomer(ModelMap view) {
-        view.addAttribute("projectName", "Grocery Store");
-        Customer customer = new Customer();
-        view.addAttribute("customer", customer);
-        return ("newcustomer");
-    }
-
     @RequestMapping(value = "/new", method = RequestMethod.POST)
-    public String saveNewCustomer(ModelMap view,
-            @Valid Customer customer,
-            BindingResult bindingResult) {
-        view.addAttribute("projectName", "Grocery Store");
-        if (bindingResult.hasErrors()) {
-            view.addAttribute("error", "There are errors on the form!");
-        } else {
-            customerService.save(customer);
+    public ResponseEntity saveNewCustomer(@RequestBody Customer customer) {
+
+        customerService.save(customer);
+
+        return (new ResponseEntity(HttpStatus.OK));
+    }
+
+    // B1. /api/customer/{id}/update or - prefferred way
+    //  B. id, {fields to be changed} e.g. {"email": "xxx@yyy.zzz"}
+    @PutMapping("/{id}/update")
+    public ResponseEntity saveUpdateCustomer(@PathVariable String id, @RequestBody Customer customer) {
+        Customer tempCustomer = customerService.findById(Integer.parseInt(id));
+        if (customer.getFirstName() != null) {
+            tempCustomer.setFirstName(customer.getFirstName());
         }
-
-        return ("newcustomer");
-    }
-
-    @GetMapping("/update/{id}")
-    public String updateCustomer(ModelMap view, @PathVariable int id) {
-        Customer customer = customerService.findById(id);
-        view.addAttribute("customer", customer);
-        return ("updatecustomer");
-    }
-
-    @PostMapping("/update/saveupdate")
-    public String saveUpdateCustomer(ModelMap view, Customer customer) {
-        view.addAttribute("projectName", "Grocery Store");
-        customerService.update(customer);
-        return ("redirect:/login");
-    }
-
-    @GetMapping("/delete/{id}")
-    public String deleteCustomer(ModelMap view, @PathVariable int id) {
-        if (customerService.delete(id)) {
-            view.addAttribute("msg", "Customer with id:" + id + "deleted successfuly!");
-            return ("redirect:/login");
+        if (customer.getLastName() != null) {
+            tempCustomer.setLastName(customer.getLastName());
         }
-        view.addAttribute("msg", "Customer with id:" + id + "was not deleted!!!");
-        return ("redirect:/login");
+        if (customer.getTel() != null) {
+            tempCustomer.setTel(customer.getTel());
+        }
+        if (customer.getEmail() != null) {
+            tempCustomer.setEmail(customer.getEmail());
+        }
+        customerService.update(tempCustomer);
+        return (new ResponseEntity(HttpStatus.OK));
     }
+    
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity deleteCustomer(@PathVariable String id) {
+        Customer tempCustomer = customerService.findById(Integer.parseInt(id));
+        if(tempCustomer != null) {
+            customerService.delete(Integer.parseInt(id));
+            return(new ResponseEntity(HttpStatus.OK));
+        }
+        return(new ResponseEntity(HttpStatus.NOT_FOUND));
+        
+    }
+    
 
     @GetMapping("/findbynames/{firstName}/{lastName}")
     public String findByFirstLastNames(ModelMap view, @PathVariable String firstName, @PathVariable String lastName) {
